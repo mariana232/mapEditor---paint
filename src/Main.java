d634c7f673d05a46349dba48ef0c9ee43b8829d1
helghast <m.a.chambel@gmail.com>
2016-02-25 14:49:09 +0000

    Merged master branch

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
 
 /**
  * Created by Mariana on 25/02/16.
@@ -11,14 +6,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-
-
-
-
-        //Load existing map from file
-        //String filePath = "/Users/codecadet/Desktop/mapTest.txt";
-        //Map map = new Map(filePath);
 
 
         //check Main arguments and choose to start an empty map or not
