199e0db941d14f42a3a38aa66da9d68c747fa84b
helghast <m.a.chambel@gmail.com>
2016-02-25 13:09:10 +0000

    added config, changed main and map, map includes open,save dialog

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
 /**
  * Created by Mariana on 25/02/16.
  */
 public class Main {
+
+
     public static void main(String[] args) {
-        Field.init(25,25,20);
+
+
+
+
+
+        //Load existing map from file
+        //String filePath = "/Users/codecadet/Desktop/mapTest.txt";
+        //Map map = new Map(filePath);
+
+
+        //check Main arguments and choose to start an empty map or not
+        if(args.length == 0){
+            Map map = new Map();
+        }else{
+            Map map = new Map(args[0]);
+        }
+
+
     }
+
+
+
+
 }
--- src/Map.java
+++ src/Map.java
@@ -1,5 +1,198 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
 /**
  * Created by codecadet on 25/02/16.
  */
 public class Map {
+
+
+    public Map(){
+
+        //init empty field
+        loadMenu();
+    }
+
+    public Map(String FilePath){
+
+        //load field with file stream
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    private void loadMenu(){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Turn off metal's use of bold fonts
+                UIManager.put("swing.boldMetal", Boolean.FALSE);
+                Menu mainMenu = new Menu();
+                mainMenu.createAndShowGUI();
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+    private class Menu extends JPanel
+            implements ActionListener {
+        static private final String newline = "\n";
+        JButton openButton, saveButton, newButton;
+        JTextArea log;
+        JFileChooser fc;
+
+        public Menu() {
+            super(new BorderLayout());
+
+            //Create the log first, because the action listeners
+            //need to refer to it.
+            log = new JTextArea(5, 20);
+            log.setMargin(new Insets(5, 5, 5, 5));
+            log.setEditable(false);
+            JScrollPane logScrollPane = new JScrollPane(log);
+
+            //Create a file chooser
+            fc = new JFileChooser();
+
+            //Uncomment one of the following lines to try a different
+            //file selection mode.  The first allows just directories
+            //to be selected (and, at least in the Java look and feel,
+            //shown).  The second allows both files and directories
+            //to be selected.  If you leave these lines commented out,
+            //then the default mode (FILES_ONLY) will be used.
+            //
+            //fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            //fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+            //Create the open button.  We use the image from the JLF
+            //Graphics Repository (but we extracted it from the jar).
+            openButton = new JButton("Open",
+                    createImageIcon("images/Open16.gif"));
+            openButton.addActionListener(this);
+
+            //Create the save button.  We use the image from the JLF
+            //Graphics Repository (but we extracted it from the jar).
+            saveButton = new JButton("Save",
+                    createImageIcon("images/Save16.gif"));
+            saveButton.addActionListener(this);
+
+            newButton = new JButton("Create",
+                    createImageIcon("images/Open16.gif"));
+            newButton.addActionListener(this);
+
+
+            //For layout purposes, put the buttons in a separate panel
+            JPanel buttonPanel = new JPanel(); //use FlowLayout
+            buttonPanel.add(openButton,0);
+            buttonPanel.add(saveButton,1);
+            buttonPanel.add(newButton,2);
+
+            //Add the buttons and the log to this panel.
+            add(buttonPanel, BorderLayout.PAGE_START);
+            add(logScrollPane, BorderLayout.CENTER);
+
+
+            log.append("\nCreated By:\n");
+            log.append("Jairson Souza\n");
+            log.append("Mariana Pereira\n");
+            log.append("João Romero\n");
+            log.append("Miguel Chambel\n");
+        }
+
+        public void actionPerformed(ActionEvent e) {
+
+            //Handle open button action.
+            if (e.getSource() == openButton) {
+                int returnVal = fc.showOpenDialog(Menu.this);
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    File file = fc.getSelectedFile();
+                    //This is where a real application would open the file.
+                    log.append("Opening: " + file.getName() + "." + newline);
+                } else {
+                    log.append("Open command cancelled by user." + newline);
+                }
+                log.setCaretPosition(log.getDocument().getLength());
+
+                //Handle save button action.
+            } else if (e.getSource() == saveButton) {
+                int returnVal = fc.showSaveDialog(Menu.this);
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    File file = fc.getSelectedFile();
+                    //This is where a real application would save the file.
+                    log.append("Saving: " + file.getName() + "." + newline);
+
+
+                } else {
+                    log.append("Save command cancelled by user." + newline);
+                }
+                log.setCaretPosition(log.getDocument().getLength());
+
+
+            }else if (e.getSource() == newButton) {
+                Field field = new Field();
+                field.init(25,25,20);
+                log.append("creating new Map");
+            }
+
+
+        }
+
+        /**
+         * Returns an ImageIcon, or null if the path was invalid.
+         */
+        protected ImageIcon createImageIcon(String path) {
+            java.net.URL imgURL = Menu.class.getResource(path);
+            if (imgURL != null) {
+                return new ImageIcon(imgURL);
+            } else {
+                System.err.println("Couldn't find file: " + path);
+
+                return null;
+            }
+        }
+
+        /**
+         * Create the GUI and show it.  For thread safety,
+         * this method should be invoked from the
+         * event dispatch thread.
+         */
+        private void createAndShowGUI() {
+            //Create and set up the window.
+            JFrame frame = new JFrame("Map Editor");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            //Add content to the window.
+            frame.add(new Menu());
+
+            //Display the window.
+            frame.pack();
+            frame.setVisible(true);
+        }
+
+
+    }
+
+
+
 }
